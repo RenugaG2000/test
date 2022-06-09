851639112e73215d4221d881be229635d0f4238d
RenugaG2000 <92309517+RenugaG2000@users.noreply.github.com>
2022-06-09 16:23:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+/*const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+
+let num = 255;
+let x = 2;
+x **= 4;
+console.log(x);
+alert(num.toString());
+alert(123456..toString(36));
+
+console.log(Math.floor(3.6));
+alert(0.1+0.2);
+
+let num1 = +prompt("Enter a num 1", "");
+let num2 = +prompt("Enter a num2", "");
+
+alert(num1 + num2); */
+
+// Task
+
+//alert( null || 2 || undefined ); //2
+
+//alert( alert(1) || 2 || alert(3)); // 1  \n 2
+
+//alert( 1 && null && 2); // null
+
+//alert( alert(1) && alert(2)); // 1 \n undefined
+
+//alert( null || 2 && 1 || 4); // 3 AND has higher precedance
+
+// write the condition for age between 14 to 90
+
+//var age = prompt("Enter your age","");
+
+// if( age >= 14 && age <= 90){
+//     alert("welcome");
+// }else{
+//     alert("Not allow");
+// }
+
+// if( !(age >= 14 && age <= 90)){
+//     alert("welcome");
+// }else{
+//     alert("Not allow");
+// }
+
+// check
+
+// if( -1 || 0){
+//     alert("first");
+// }
+// if( -1 && 0){
+//     alert("second");
+// }
+// if( null || -1 && 1){
+//     alert("third");
+// }
+// first \n third
+
+// Login
+
+// var userName = prompt("Enter your name", "Admin");
+
+// if(userName == "Admin"){
+
+//     var password = prompt("Enter your password","TheMaster");
+//     if(password == "TheMaster"){
+//         alert("Welcome!");
+//     }else if(password === "" || password === null){
+//         alert("Cancled");
+//     }else{
+//         alert("Wrong password");
+//     }
+
+// }else if(userName === "" || userName === null){
+//     alert("Cancled");
+// }else{
+//     alert("I don't know you!");
+// }
